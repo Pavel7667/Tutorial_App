640b2a2b31546f7b35e7f35009c371e0de74f66a
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-04-06 21:09:42 +0300

    Toast vs Snackbar

--- app/src/main/java/tutorial/app/MainActivity.kt
+++ app/src/main/java/tutorial/app/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         val editText : EditText = findViewById(R.id.EditTextID)
         btn.setOnClickListener {
             val input = editText.text.toString()
+            // they work together
             Toast.makeText(it.context,input,Toast.LENGTH_SHORT).show()
             Snackbar.make(it,input,Snackbar.LENGTH_SHORT).show()
         }
